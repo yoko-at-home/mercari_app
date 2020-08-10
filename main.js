b59fe64e488ac4783196c844b317f6e1fd26427d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-08-10 10:28:38 +0900

    JavaScriptカウント試行錯誤

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-var i = 0;
+// var i = 0;
 
-function Count() {
-  document.getElementById("count").innerHTML = i++ + 1;
-}
+// function Count() {
+//   document.getElementById("count").innerHTML = i++ + 1;
+// }
+
+// function changeColor() {
+//   var elem = document.getElementById("heart");
+//   elem.style.color = "red";
+// }
+
+//
+const heart = document.querySelector("#heart");
+const count = document.querySelector("#count");
 
 function changeColor() {
-  var elem = document.getElementById("heart");
-  elem.style.color = "red";
+  heart.style.color = "red";
 }
+
+// var i = 0;
+// function count() {
+//   innerHTML = i++ + 1;
+// }
+
+heart.addEventListener("click", changeColor);
+count.addEventListener("click", count);
